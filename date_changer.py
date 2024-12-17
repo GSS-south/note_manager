ca121efb656b3bc58bb13afd371f7b0fbedcc4c2
GSS-south <gepass1997@gmail.com>
2024-12-17 23:14:20 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,8 @@
+created_date = "17.12.2024"
+issue_date = "17.01.2025"
+temp_created_date = created_date [0:5]
+temp_issue_date = issue_date [0:5]
+print("Created date:", created_date)
+print("Issue date:", issue_date)
+print("Temp created date:", temp_created_date)
+print("Temr issue date:", temp_issue_date)
---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "GSS-south"
+title = "Grade 1"
+content = "Stage 1"
+status = "Task 1"
+created_date = "17.12.2024"
+issue_date = "17.01.2025"
+print("Username:", username)
+print("Title:", title)
+print("Content:", content)
+print("Status:", status)
+print("Created date:", created_date)
+print("Issue date:", issue_date)
