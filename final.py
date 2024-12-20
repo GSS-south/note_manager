64281045e4ac6db2ecc4c823bab492554a915f74
GSS-south <gepass1997@gmail.com>
2024-12-20 16:14:38 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,21 @@
+username = input ("Введите имя пользователя ")
+title = input ("Введите заголовок заметки ")
+content = input ("Введите содержание заметки ")
+status = input ("Введите статус заметки ")
+created_date = input ('Введите дату создания заметки в формате "дд-мм-гггг" ')
+issue_date = input ('Введите дату завершения заметки в формате "дд-мм-гггг" ')
+changed_date = input('Введите дату изменения заметки в формате "дд-мм-гггг" ')
+temp_created_date = created_date [0:5]
+temp_issue_date = issue_date [0:5]
+title_1 = input("Grade 1")
+title_2 = input("Grade 2")
+titles = [title_1, title_2,]
+
+note = [  "Имя пользователя: ", username,
+        "Содержание заметки: ", content,
+                    "Статус: ", status,
+             "Дата создания: ", created_date,
+            "Дата изменения: ", changed_date,
+        [title_1, title_2]]
+print(note)
+
