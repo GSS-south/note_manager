5816bb6c8886cea2165d9143db9dd5d841c73be2
GSS-south <gepass1997@gmail.com>
2024-12-20 16:22:49 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -2,7 +2,7 @@ created_date = "17.12.2024"
 issue_date = "17.01.2025"
 temp_created_date = created_date [0:5]
 temp_issue_date = issue_date [0:5]
-print("Created date:", created_date)
-print("Issue date:", issue_date)
-print("Temp created date:", temp_created_date)
-print("Temp issue date:", temp_issue_date)
+print("Дата создания:", created_date)
+print("Дедлайн:", issue_date)
+print("Дата создания:", temp_created_date)
+print("Дедлайн:", temp_issue_date)
--- greetings.py
+++ greetings.py
@@ -4,9 +4,9 @@ content = "Stage 1"
 status = "Task 1"
 created_date = "17.12.2024"
 issue_date = "17.01.2025"
-print("Username:", username)
-print("Title:", title)
-print("Content:", content)
-print("Status:", status)
-print("Created date:", created_date)
-print("Issue date:", issue_date)
+print("Имя пользователя: ", username)
+print("Заголовок заметки: ", title)
+print("Описание заметки: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки в формате день-месяц-год: ", created_date)
+print("Дата истечения заметки (дедлайн) в формате день-месяц-год:", issue_date)
